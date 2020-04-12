399a42a5aae6b493cb74ef3b0501c72b69694f35
joelgm2000 <joelgarciamaestrejgm@gmail.com>
2020-04-12 03:00:31 -0500

    HTML NUEVOS

--- exposoftwaredotnet/ClientApp/src/app/app-routing.module.ts
+++ exposoftwaredotnet/ClientApp/src/app/app-routing.module.ts
@@ -2,12 +2,41 @@ import { ProyectoRegistroComponent } from './inscripcion/proyecto-registro/proye
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-
+import { LoginLiderProyectoComponent } from './login-lider-proyecto/login-lider-proyecto.component';
+import { LoginDocenteEvaluadorComponent } from './login-docente-evaluador/login-docente-evaluador.component';
+import { LoginComiteEvaluadorComponent } from './login-comite-evaluador/login-comite-evaluador.component';
+import { InformacionComponent } from './informacion/informacion/informacion.component';
 const routes: Routes = [
   {
-  path: 'proyectoRegistro',
-  component: ProyectoRegistroComponent
+    path: 'proyectoRegistro',
+    component: ProyectoRegistroComponent
+  },
+
+  {
+    path: 'loginLider',
+    component: LoginLiderProyectoComponent
+
+  },
+
+  {
+    path: 'LoginDocenteEvaluador',
+    component: LoginDocenteEvaluadorComponent
+
+  },
+
+  {
+    path: 'LoginComiteEvaluador',
+    component: LoginComiteEvaluadorComponent
+
+  },
+  
+  {
+    path: 'Informacion',
+    component: InformacionComponent
+
   }
+  
+
 ];
 
 @NgModule({
@@ -16,6 +45,6 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- exposoftwaredotnet/ClientApp/src/app/app.module.ts
+++ exposoftwaredotnet/ClientApp/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +11,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProyectoRegistroComponent } from './inscripcion/proyecto-registro/proyecto-registro.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginLiderProyectoComponent } from './login-lider-proyecto/login-lider-proyecto.component';
+import { LoginComiteEvaluadorComponent } from './login-comite-evaluador/login-comite-evaluador.component';
+import { LoginDocenteEvaluadorComponent } from './login-docente-evaluador/login-docente-evaluador.component';
+import { InformacionComponent } from './informacion/informacion/informacion.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,11 @@ import { AppRoutingModule } from './app-routing.module';
     CounterComponent,
     FetchDataComponent,
     FooterComponent,
-    ProyectoRegistroComponent
+    ProyectoRegistroComponent,
+    LoginLiderProyectoComponent,
+    LoginComiteEvaluadorComponent,
+    LoginDocenteEvaluadorComponent,
+    InformacionComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
